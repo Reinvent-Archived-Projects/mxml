c19fff2f31d7ada73d5f7c61ea4e18039df4825c
Alejandro Isaza <aisaza@venturemedia.com>
2015-03-13 10:43:38 -0700

    Fix crash when increasing the number of staves
    Fix venturemedia/intune-ios#418

--- src/mxml/dom/Attributes.cpp
+++ src/mxml/dom/Attributes.cpp
@@ -13,28 +13,17 @@ void Attributes::setStaves(Optional<int> staves) {
     if (staves > kMaximumStaves)
         staves = kMaximumStaves;
 
-    if (_staves > 0 && staves > _staves) {
-        auto& lastClef = _clefs.back();
-        auto& lastKey = _keys.back();
+    while (_clefs.size() < staves)
+        _clefs.emplace_back();
 
-        for (int i = _staves; i < staves; i += 1) {
-            if (lastClef) {
-                _clefs.emplace_back(new Clef(*lastClef));
-                _clefs.back()->setNumber(i);
-            } else {
-                _clefs.emplace_back();
-            }
-
-            if (lastKey) {
-                _keys.emplace_back(new Key(*lastKey));
-                _keys.back()->setNumber(i);
-            } else {
-                _keys.emplace_back();
-            }
+    auto reuseKey = _keys.back().get();
+    while (_keys.size() < staves) {
+        if (reuseKey) {
+            _keys.emplace_back(new Key(*reuseKey));
+            _keys.back()->setNumber(static_cast<int>(_keys.size()));
+        } else {
+            _keys.emplace_back();
         }
-    } else {
-        _clefs.resize(staves);
-        _keys.resize(staves);
     }
 
     _staves = staves;
@@ -42,14 +31,14 @@ void Attributes::setStaves(Optional<int> staves) {
 
 void Attributes::setClef(int number, std::unique_ptr<Clef>&& clef) {
     assert(number > 0 && number < kMaximumStaves);
-    while (_clefs.size() <= number)
+    while (_clefs.size() < number)
         _clefs.emplace_back();
     _clefs[number - 1] = std::move(clef);
 }
 
 void Attributes::setKey(int number, std::unique_ptr<Key> key) {
     assert(number > 0 && number < kMaximumStaves);
-    while (_keys.size() <= number)
+    while (_keys.size() < number)
         _keys.emplace_back();
     _keys[number - 1] = std::move(key);
 }
